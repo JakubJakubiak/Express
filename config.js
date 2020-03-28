2db9548206714421639dd507e2fbf4951837c860
Jakub Jakubiak <jebacyuu@gmail.com>
2020-03-28 21:10:34 +0100

    Express api mongoDB Test

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: 'mongodb+srv://admin:R6HVwzrdc1vHGEzI@cluster0-tyepi.mongodb.net/test?retryWrites=true&w=majority',
+  keySession: [`keySession`],
+  maxAgeSession: 24 * 60 * 60 * 1000
+}
